214e25001bd41c6e5a2884629bef99f32c5ab728
Aar2284 <aaryankalia165@gmail.com>
2026-05-25 01:46:19 +0530

    Created Character Movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 7f;
+
+    private void Update()
+    {
+        Vector2 inputVector = new Vector2(0, 0);
+
+        if(Keyboard.current != null){
+
+            if(Keyboard.current.wKey.isPressed)
+            {
+                inputVector.y = +1;
+            }
+
+            if(Keyboard.current.sKey.isPressed)
+            {
+                inputVector.y = -1;
+            }
+
+            if(Keyboard.current.aKey.isPressed)
+            {
+                inputVector.x = -1;
+            }
+
+            if(Keyboard.current.dKey.isPressed)
+            {
+                inputVector.x = +1;
+            }
+        }
+
+        inputVector = inputVector.normalized;
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        Debug.Log(Time.deltaTime);
+    }
+}
