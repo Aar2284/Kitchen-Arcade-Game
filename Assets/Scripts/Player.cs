a2e3ad32fb14dc7a17937a5fbc2e88c788a5490b
Aar2284 <aaryankalia165@gmail.com>
2026-05-27 02:02:51 +0530

    Created Clear Counter

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,13 +5,44 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private GameInput gameInput;
-    
+    [SerializeField] private LayerMask countersLayerMask;
     private bool isWalking;
+    private Vector3 lastInteractDir;
 
     private void Update()
     {
-        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        HandleMovement();
+        HandelInteractions();
+    }
 
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+    private void HandelInteractions()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        if(moveDir != lastInteractDir)
+        {
+            lastInteractDir = moveDir;
+        }
+        float interactDistance = 2f;
+
+        if(Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask))
+        {
+            if(raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                //Has ClearCounter
+                clearCounter.Interact();
+            }
+        }
+    }
+
+    private void HandleMovement()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
         float moveDistance = moveSpeed * Time.deltaTime;
@@ -53,7 +84,7 @@ public class Player : MonoBehaviour
                 }
             }
         }
-            
+
         if(canMove)
         {
             transform.position += moveDir * moveDistance;
@@ -65,10 +96,5 @@ public class Player : MonoBehaviour
             float rotateSpeed = 10f;
             transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
         }
-    }
-
-    public bool IsWalking()
-    {
-        return isWalking;
     }
 }
