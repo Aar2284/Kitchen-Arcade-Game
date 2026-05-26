c14a6418f6620696057e25efef0886757a80db10
Aar2284 <aaryankalia165@gmail.com>
2026-05-26 23:49:39 +0530

    Created PlayerInputActions

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -3,32 +3,16 @@ using UnityEngine.InputSystem;
 
 public class GameInput : MonoBehaviour
 {
+    PlayerInputActions playerInputActions;
+    private void Awake()
+    {
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+    }
+
     public Vector2 GetMovementVectorNormalized()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-
-        if(Keyboard.current != null){
-
-            if(Keyboard.current.wKey.isPressed)
-            {
-                inputVector.y = +1;
-            }
-
-            if(Keyboard.current.sKey.isPressed)
-            {
-                inputVector.y = -1;
-            }
-
-            if(Keyboard.current.aKey.isPressed)
-            {
-                inputVector.x = -1;
-            }
-
-            if(Keyboard.current.dKey.isPressed)
-            {
-                inputVector.x = +1;
-            }
-        }
+        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();;
 
         inputVector = inputVector.normalized;
 
